79a3b0fc37e376cf7532409977b8847d98d69a7b
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2026-03-23 16:26:03 +0800

    improve resume toolbar

--- src/features/my-resume/components/my-resume-toolbar.tsx
+++ src/features/my-resume/components/my-resume-toolbar.tsx
@@ -1,4 +1,4 @@
-import { ExternalLink } from 'lucide-react'
+import { Download, ExternalLink, Monitor } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group'
@@ -8,7 +8,7 @@ export default function MyResumeToolbar() {
         <div className="sticky bottom-4 z-50 mb-8 flex w-fit items-center gap-1 rounded-2xl bg-[#222222] p-2 text-white shadow-2xl">
             {/* Logo Section */}
             <div className="flex h-12 w-12 items-center justify-center rounded-xl bg-[#1a1a1a] text-xl font-bold">
-                W<span className="mb-2 self-end text-sm">.</span>
+                AG<span className="mb-2 self-end text-sm">.</span>
             </div>
 
             {/* Navigation Group */}
@@ -21,26 +21,30 @@ export default function MyResumeToolbar() {
                     value="creator"
                     className="rounded-lg border border-transparent px-4 py-2 text-xs font-medium hover:bg-transparent data-[state=on]:border-amber-200/50 data-[state=on]:text-amber-200"
                 >
-                    Creator
+                    Total Page
                 </ToggleGroupItem>
                 <ToggleGroupItem
                     value="font"
                     className="rounded-lg border border-transparent px-4 py-2 text-xs font-medium text-gray-400 hover:text-white"
                 >
-                    Font & Color
-                </ToggleGroupItem>
-                <ToggleGroupItem value="details" className="...">
-                    Details
-                </ToggleGroupItem>
-                <ToggleGroupItem value="score" className="...">
-                    Score
+                    Page 1 of 2
                 </ToggleGroupItem>
             </ToggleGroup>
 
             {/* CTA Button */}
-            <Button className="ml-1 h-12 rounded-xl bg-[#FFF38A] px-6 font-bold text-black hover:bg-[#ffe945]">
-                Visit Site
-            </Button>
+            <div className="flex gap-2">
+                {/* Preview Button */}
+                <Button className="ml-1 h-12 rounded-xl bg-[#FFF38A] px-6 font-bold text-black hover:bg-[#ffe945]">
+                    <Monitor className="mr-2 h-5 w-5" />
+                    Preview
+                </Button>
+
+                {/* Download Button */}
+                <Button className="ml-1 h-12 rounded-xl bg-[#FFF38A] px-6 font-bold text-black hover:bg-[#ffe945]">
+                    <Download className="mr-2 h-5 w-5" />
+                    Download
+                </Button>
+            </div>
         </div>
     )
 }
